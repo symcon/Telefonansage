5d8110a24e4f7d329206b400f0203e5e8d91af30
Till Brede <tb@symcon.de>
2020-08-06 12:08:07 +0200

    added validation test

---
+++ tests/TelefonansageValidationTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+include_once __DIR__ . '/stubs/Validator.php';
+class TelefonansageValidationTest extends TestCaseSymconValidation
+{
+    public function testValidateTelefonansage(): void
+    {
+        $this->validateLibrary(__DIR__ . '/..');
+    }
+    public function testValidateTelefonansageModule(): void
+    {
+        $this->validateModule(__DIR__ . '/../Telefonansage');
+    }
+}
